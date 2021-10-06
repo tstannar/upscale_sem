e9fee387f26d090d4a65a669820124f5893026ea
Tyler Stannard <tyler.jaros.stannard@gmail.com>
2021-10-06 11:23:41 -0700

    Adding more analysis.

--- drln_on_sem_analysis.py
+++ drln_on_sem_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
 
 # Import the results from all the tests of the SEM images on the
 # Pretrained DLNR super resolution model.
@@ -8,6 +9,11 @@ results_df = pd.read_csv(r'../Results/results_list.csv')
 
 # Drop duplicates in case any images were run twice during starting and stopping.
 results_df = results_df.drop(columns = 'Unnamed: 0').drop_duplicates()
+
+# Calculate the difference in PSNR and SSIM for each image.
+# We want to see increases in PSNR and SSIM.
+results_df['psnr_diff'] = results_df['dlnr_psnr'] - results_df['bicubic_psnr']
+results_df['ssim_diff'] = results_df['dlnr_ssim'] - results_df['bicubic_ssim']
 
 # Separate out the category to see how SEM sample type affects upscaling performance.
 results_df['Category'] = results_df.loc[:,'sem_file'].\
@@ -17,15 +23,16 @@ results_df['Category'] = results_df.loc[:,'sem_file'].\
 # Drop any NAN or INF image results.
 results_df = results_df.replace([np.inf, -np.inf], np.nan).dropna(axis=0,how='any')
 
-# Calculate the difference in PSNR and SSIM for each image.
-# We want to see increases in PSNR and SSIM.
-results_df['psnr_diff'] = results_df['dlnr_psnr'] - results_df['bicubic_psnr']
-results_df['ssim_diff'] = results_df['dlnr_ssim'] - results_df['bicubic_ssim']
+
 
 # Visualize the distribution of PSNR and SSIM across all images.
-results_df[['psnr_diff']].hist()
-results_df[['ssim_diff']].hist()
-
+figure, (ax1,ax2) = plt.subplots(2,1)
+results_df[['psnr_diff']].hist(ax=ax1)
+ax1.set_title('Histogram of PSNR Difference \n Between Bicubic and DLRN')
+results_df[['ssim_diff']].hist(ax=ax2)
+ax2.set_title('Histogram of SSIM Difference \n Between Bicubic and DLRN')
+plt.tight_layout()
+figure.set_size_inches(6,9)
 # Compute descriptive statistics of all the results.
 desc_df = results_df.groupby(by='Category').describe()
 
@@ -46,7 +53,11 @@ stat_df = pd.concat(stat_list,axis=1)
 # Make a final summary table of P-values and mean differences.
 summary_df = pd.concat([desc_df[('dlnr_psnr','count')].rename('count'),
                         rel_desc_df,stat_df],axis=1)
+summary_df.to_csv(r'../Results/results_summary.csv')
+
 # print the output
 print(summary_df)
-
+# .set_index(keys='sem_file')
+best_psnr = results_df.groupby(by='Category').apply(lambda x: x.sort_values(by='psnr_diff').iloc[[0,-1],:])
+best_ssim = results_df.groupby(by='Category').apply(lambda x: x.sort_values(by='ssim_diff').iloc[[0,-1],:])
 
--- validate_drln.py
+++ validate_drln.py
@@ -27,37 +27,49 @@ cuda_gpu = torch.device('cuda')
 model = DrlnModel.from_pretrained('eugenesiow/drln-bam', scale=scale)      # scale 2, 3 and 4 models available
 
 
-# Loop through each image and calculate to make a dataframe of SSIM, PSNR.
-start = time.time()
-results_list = list()
-for sem_file in sem_files:
-    # Load the images.
-    image_a = Image.open(open(sem_file,'rb'))
-    image = asarray(image_a)
+def scale_image_for_test(image):
+    """
+    # Upscales images on GPU.
+    # Works within the validate_drln code's context, depends on the model definition within the script.
+    :param sem_file: Takes the SEM file name.
+    :return: Returns the raw image, the upscaled image from DLRN and the bicubic upscaled image.
+    """
     # print(image.shape)
     # Make sure image is not excessively large. If so, cut it down so its under 1024 pixels long on the longer edge.
-    if image.shape[1]>1050:
-        down_scale = image.shape[1]/1024
+    if image.shape[1] > 1050:
+        down_scale = image.shape[1] / 1024
         image = cv2.resize(image, (int(image.shape[1] / down_scale), int(image.shape[0] / down_scale)),
-                              interpolation=cv2.INTER_AREA)
+                           interpolation=cv2.INTER_AREA)
     # Crop the image if it has an odd shape that would not scale up to the identical size.
-    image = image[0:clean_scale_crop(image.shape[0],scale), 0:clean_scale_crop(image.shape[1],scale),:]
+    image = image[0:clean_scale_crop(image.shape[0], scale), 0:clean_scale_crop(image.shape[1], scale), :]
     # Downscale the image using area interpolation to check the upscale performance against the original.
-    image_in = cv2.resize(image,(int(image.shape[1]/scale),int(image.shape[0]/scale)),interpolation = cv2.INTER_AREA)
+    image_in = cv2.resize(image, (int(image.shape[1] / scale), int(image.shape[0] / scale)),
+                          interpolation=cv2.INTER_AREA)
     # Put the image back into a PIL JPG format
     image_in = Image.fromarray(image_in)
     # Prepare the image for use in Pytorch.
     inputs = ImageLoader.load_image(image_in)
     # Upscale the image using pytorch for GPU.
-    preds = model_inputs(inputs,model,cuda_gpu)
+    preds = model_inputs(inputs, model, cuda_gpu)
 
     # Convert the pytorch images to arrays.
     input_im = tensor_to_array(inputs)
     output_im = tensor_to_array(preds)
 
     # Use bicubic interpolation to upsample the input image to see if there is an improvement.
-    input_resize = cv2.resize(input_im, (output_im.shape[1], output_im.shape[0]), interpolation = cv2.INTER_CUBIC)
+    input_resize = cv2.resize(input_im, (output_im.shape[1], output_im.shape[0]), interpolation=cv2.INTER_CUBIC)
+    return image, output_im, input_resize
 
+
+# Loop through each image and calculate to make a dataframe of SSIM, PSNR.
+start = time.time()
+results_list = list()
+for sem_file in sem_files:
+    # Load the images.
+    image_a = Image.open(open(sem_file, 'rb'))
+    image = asarray(image_a)
+
+    image, output_im, input_resize = scale_image_for_test(image)
     # Compute the psnr and ssim metrics for the dlnr and bicubic upsampling images.
     dlnr_psnr = psnr(image, output_im)
     dlnr_ssim = ssim(image, output_im, win_size=3)
@@ -73,13 +85,44 @@ for sem_file in sem_files:
 end = time.time()
 print(end-start)
 
-# Make the dictionary list into a dataframe and save the results.
+# Make the dictionary list into a dataframe.
 results_df = pd.DataFrame.from_dict(results_list)
+
+# Save the results.
 results_df.to_csv(r'../Results/results_list.csv')
 
-# Plot the results to compare the reference to the DLNR and bicubic upsampling results.
-# fig, (ax1, ax2,ax3 ) = plt.subplots(1,3)
-# ax1.imshow(image)
-# ax2.imshow(output_im.astype(int).clip(0,255))
-# ax3.imshow(input_resize.astype(int).clip(0,255))
+######### Visualize the results.
+results_df = pd.read_csv(r'../Results/results_list.csv')
 
+# Calculate the difference in PSNR and SSIM for each image.
+# We want to see increases in PSNR and SSIM.
+results_df['psnr_diff'] = results_df['dlnr_psnr'] - results_df['bicubic_psnr']
+results_df['ssim_diff'] = results_df['dlnr_ssim'] - results_df['bicubic_ssim']
+
+results_df['Category'] = results_df.loc[:,'sem_file'].\
+                             astype(str).str.rsplit('/',expand=True,n=2).\
+                             loc[:,1].rename('Category')
+
+for stat in ['psnr','ssim']:
+    best_worst_df = results_df.groupby(by='Category').\
+        apply(lambda x: x.sort_values(by=stat+'_diff').iloc[[0,-1],:]).drop(columns='Category')
+
+    for cat,cat_df in best_worst_df.groupby(by='Category'):
+        print(cat)
+        fig, (axu,axl) = plt.subplots(2, 3)
+        for ax, sem_file, bw in zip([axu,axl],cat_df['sem_file'].to_list(),['Best', 'Worst']):
+            # Load the images.
+            image_a = Image.open(open(sem_file, 'rb'))
+            image = asarray(image_a)
+            center = [int(image.shape[0]/2), int(image.shape[1]/2)]
+            image = image[center[0]-100:center[0]+100,center[1]-100:center[1]+100]
+
+            image, output_im, input_resize = scale_image_for_test(image)
+            # Plot the results to compare the reference to the DLNR and bicubic up-sampling results.
+            ax[0].imshow(image)
+            ax[0].set_ylabel(bw)
+            ax[1].imshow(output_im.astype(int).clip(0,255))
+            ax[2].imshow(input_resize.astype(int).clip(0,255))
+        fig.suptitle('Best and Worst '+ stat.upper() + ' Change')
+        fig.savefig('../Results/'+cat+'_'+stat+'.png')
+        plt.close(fig)
